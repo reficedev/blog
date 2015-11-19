6d0f97ab7d2515de82703e8dbd68f2eb0e48d608
unknown <jh@STN-W7-35-JH.ice-dev.lan>
2015-11-19 09:12:50 +0100

    Pour afficher les réseaux sociaux sur la fiche Google My Business

---
+++ profiles_reseaux_sociaux.js
@@ -0,0 +1,19 @@
+/**
+ * Pour afficher les réseaux sociaux sur la fiche Google My Business, coe à mettre dans la partie <head>
+ *
+ * @category   Référencement
+ * @author     Joffrey HABAI <jh@ice-dev.com>
+ * @date       2015 02 03
+ */
+ 
+ <script type="application/ld+json">
+	{ 
+	  "@context" : "http://schema.org",
+	  "@type" : "Organization",
+	  "name" : "Your Organization Name",
+	  "url" : "http://www.your-site.com",
+	  "sameAs" : [ "http://www.facebook.com/your-profile",
+	  "http://www.twitter.com/yourProfile",
+	  "http://plus.google.com/your_profile"]
+	}
+</script>
